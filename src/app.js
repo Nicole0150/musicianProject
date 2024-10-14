1638c0f08842519f43a9dc3e419d7a15916792da
Nicole0150 <nicole.williamson@barlcays.com>
2024-10-14 13:16:12 +0100

    extension work without stretch tasks complete

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ const { db } = require("../db/connection")
 
 const port = 3000;
 
+app.get("/musicians", async (req, res) => {
+    const musicians = await Musician.findAll({});
+    res.json(musicians);
+})
+
 //TODO: Create a GET /musicians route to return all musicians 
 
 
